c0fd2ac923bfd864a7fa36f21812fa661749ff99
baibhavrajput <baibhavrajputt@gmail.com>
2022-12-30 14:52:11 +0530

    initial commit

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,15 +1,26 @@
+import { useState } from "react";
+
 function SearchBar(props) {
+  const onSubmit = props.onSubmit;
 
-    const onSubmit = props.onSubmit;
-    const onClickHandler = () => {
-        onSubmit('cars');
-    };
+  const [searchTerm, setSearchTerm] = useState("");
 
-    return (
-        <div>
-            <input/>
-        </div>
-    )
+  const onFormSubmitHandler = (event) => {
+    event.preventDefault();
+    onSubmit("cars");
+  };
+
+  const onChangeHandler = (event) => {
+    setSearchTerm(event.target.value);
+  };
+
+  return (
+    <div>
+      <form onSubmit={onFormSubmitHandler}>
+        <input value={searchTerm} onChange={onChangeHandler} />
+      </form>
+    </div>
+  );
 }
 
 export default SearchBar;
